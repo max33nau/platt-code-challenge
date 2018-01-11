ccb224e627f6378daeb46b4412ed260b8f351810
Max Jacobsen <mtjacobsen33@gmail.com>
2018-01-10 16:41:54 -0800

    adds content info for mobile

--- src/components/app-content.vue
+++ src/components/app-content.vue
@@ -4,6 +4,15 @@
     <img class="content-box-shadow content-panel todays-deal-img" src="../assets/img/todays-deal.png" alt="Todays Deal"/>
     <content-plus-member class="content-box-shadow content-panel"> </content-plus-member>
     <content-recently-viewed class="content-box-shadow content-panel"> </content-recently-viewed>
+    <div class="content-info-container">
+      <content-info class="content-box-shadow content-panel" 
+        v-for="(item, index) in info"
+        :key="index"
+        :src="item.src"
+        :text="item.text"
+      >
+      </content-info>  
+    </div>
   </div>
 </template>
 
@@ -11,16 +20,42 @@
   import ContentProductOptions from './content-product-options'
   import ContentPlusMember from './content-plus-member'
   import ContentRecentlyViewed from './content-recently-viewed'
+  import ContentInfo from './content-info'
+
+  import superInfo from '../assets/img/info/super-tool-day.png'
+  import locationInfo from '../assets/img/info/ic_location_b.png'
+  import helpInfo from '../assets/img/info/ic_helpcenter.png'
+  import universityInfo from '../assets/img/info/ic_platt-u.png'
 
   export default {
     name: 'app-content',
     data: function () {
-      return {}
+      return {
+        info: [
+          {
+            src: superInfo,
+            text: 'Super Tool Day & Dynamic Data Day'
+          },
+          {
+            src: locationInfo,
+            text: 'Branch Locations'
+          },
+          {
+            src: helpInfo,
+            text: 'Help Center'
+          },
+          {
+            src: universityInfo,
+            text: 'Platt University'
+          }
+        ]
+      }
     },
     components: {
       'content-product-options': ContentProductOptions,
       'content-plus-member': ContentPlusMember,
-      'content-recently-viewed': ContentRecentlyViewed
+      'content-recently-viewed': ContentRecentlyViewed,
+      'content-info': ContentInfo
     }
   }
 </script>
@@ -51,6 +86,12 @@
     max-width: 100%;
   }
 
+  .content-info-container {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    flex-wrap: wrap;
+  }
 
 
 </style>
---
+++ src/components/content-info.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="content-info">
+    <div class="image-container">
+      <img
+        :src="src"
+      />
+    </div>
+    <div class="info-text">
+      {{text}}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'content-info',
+    props: [ 'src', 'text' ]
+  }
+</script>
+
+<style lang="scss" scoped>
+
+  .content-info {
+    display: inline-block;
+    width: 48%;
+    text-align: center;
+    padding: 20px 15px;
+    min-height: 175px;
+    position: relative;
+
+    .info-text {
+      font-weight: bold;
+      display: flex;
+      min-height: 45px;
+      align-items: center;
+      justify-content: center;
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 10px;
+    }
+  }
+
+</style>
